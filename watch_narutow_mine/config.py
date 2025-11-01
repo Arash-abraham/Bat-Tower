5d0e38128949ec3188255664d7ea8e084f0eda5b
Arash <82562395+Arash-abraham@users.noreply.github.com>
2025-11-01 17:57:57 +0330

    Delete all

--- watch_narutow_mine/config.py
+++
@@ -1,6 +0,0 @@
-# configuration, variables, etc
-def config():
-    return {
-        'WATCH_DIR': '/opt/watch_narutow/',
-        'WEBHOOK_URL': 'https://discord.com/api/webhooks/1273582725319622719/CT5wY6YcyXRYTLFLJfoxmI-333DmBPX58-d91tykMiMSZa-HcJtS_EnrjCwE7JHbygpY'
-    }
