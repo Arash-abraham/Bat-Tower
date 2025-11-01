c51c1c7a5862b4d6c24f8e92fdaa2ffc2d5f6edc
Arash <82562395+Arash-abraham@users.noreply.github.com>
2025-11-01 18:19:19 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 def Config():
     BAT_DIR =  '/home/arash/Desktop/Bat-Tower'
 
-    WEBHOOK_URL = 'https://discord.com/api/webhooks/1276892375171596310/vGt2__j_A9UFodxKcjdfEDDyv6M2XmH3HqflhdZchD_cWCxpbySxpAEnYcyO0Yoy3LXa'
+    WEBHOOK_URL = 'YOUR-WEB-HOOK'
 
     return { 'BAT_DIR' : BAT_DIR,
              'WEBHOOK_URL' : WEBHOOK_URL       
